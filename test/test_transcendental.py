0993a1ffdfffa06ce4da7147106fafe162d88ad7
hurrrsh <harshnatuskar10@gmail.com>
2025-02-12 20:08:13 +0530

    more cleanup

--- test/test_transcendental.py
+++ test/test_transcendental.py
@@ -22,8 +22,8 @@ class TestTranscendentalMath(unittest.TestCase):
       if abs(x) > 100_000_000: return
     with Context(TRANSCENDENTAL=2), np.errstate(all='ignore'):
       np.testing.assert_allclose(op[0](Tensor([x], dtype=dtypes.float64)).numpy(),
-        op[1](np.array([x], dtype=_to_np_dtype(dtypes.float64))),
-        atol=3e-2, rtol=1e-5)  # sin can have bigger atol for very big x
+                                op[1](np.array([x], dtype=_to_np_dtype(dtypes.float64))),
+                                atol=3e-2, rtol=1e-5)  # sin can have bigger atol for very big x
 
   @unittest.skipIf(getenv("MOCKGPU") and Device.DEFAULT in {"NV", "CUDA"}, "crashed")
   @given(ht.float32, strat.sampled_from([(Tensor.exp, np.exp),(Tensor.log, np.log)] +
@@ -34,8 +34,8 @@ class TestTranscendentalMath(unittest.TestCase):
       return
     with Context(TRANSCENDENTAL=2), np.errstate(all='ignore'):
       np.testing.assert_allclose(op[0](Tensor([x], dtype=dtypes.float32)).numpy(),
-        op[1](np.array([x], dtype=_to_np_dtype(dtypes.float32))),
-        atol=2e-5, rtol=1e-5)
+                                op[1](np.array([x], dtype=_to_np_dtype(dtypes.float32))),
+                                atol=2e-5, rtol=1e-5)
 
   @unittest.skipUnless(is_dtype_supported(dtypes.float16, Device.DEFAULT), f"no float16 on {Device.DEFAULT}")
   @given(ht.float16, strat.sampled_from([(Tensor.exp, np.exp), (Tensor.log, np.log)] +
@@ -46,8 +46,8 @@ class TestTranscendentalMath(unittest.TestCase):
       return
     with Context(TRANSCENDENTAL=2), np.errstate(all='ignore'):
       np.testing.assert_allclose(op[0](Tensor([x], dtype=dtypes.float16)).numpy(),
-        op[1](np.array([x], dtype=_to_np_dtype(dtypes.float16))),
-        atol=1e-2, rtol=5e-3)  # exp can have bigger rtol
+                                op[1](np.array([x], dtype=_to_np_dtype(dtypes.float16))),
+                                atol=1e-2, rtol=5e-3)  # exp can have bigger rtol
 
   @given(strat.sampled_from([(dtypes.float64, 709.5), (dtypes.float32, 88.7), (dtypes.float16, 11)]
             if Device.DEFAULT != "WEBGPU" else
@@ -55,8 +55,7 @@ class TestTranscendentalMath(unittest.TestCase):
   def test_exp_near_inf(self, dtype_x):
     # reordering compute might return inf
     dtype, x = dtype_x
-    if not is_dtype_supported(dtype):
-      return
+    if not is_dtype_supported(dtype): return
     with Context(TRANSCENDENTAL=2):
       y = Tensor([x], dtype=dtype).exp().numpy()
       expected = np.exp(np.array([x], dtype=_to_np_dtype(dtype)))
@@ -66,8 +65,7 @@ class TestFromFuzzer(unittest.TestCase):
   @given(strat.sampled_from(dtypes_float))
   @unittest.skipUnless(is_dtype_supported(dtypes.ulong), "Needs ulong")
   def test_sin(self, dtype):
-    if not is_dtype_supported(dtype):
-      return
+    if not is_dtype_supported(dtype): return
     if dtype == dtypes.float64:
       # crashes in CI CUDA
       if getenv("MOCKGPU") and Device.DEFAULT in {"NV", "CUDA"}:
@@ -84,11 +82,11 @@ class TestFromFuzzer(unittest.TestCase):
     _test_value(-35.0)
     _test_value(-25.0)
     _test_value(25.0)
-    _test_value(30.0)  # 30.0 == switch_over
+    _test_value(30.0) # 30.0 == switch_over
     _test_value(35.0)
     _test_value(0.0)
     _test_value(np.pi / 2)
-    # worst case of ulp 1.5
+     # worst case of ulp 1.5
     _test_value(np.pi * 2, unit=1.5)
 
   @given(strat.sampled_from(dtypes_float))
@@ -98,15 +96,12 @@ class TestFromFuzzer(unittest.TestCase):
     if dtype == dtypes.float64:
       # crashes in CI CUDA
       if getenv("MOCKGPU") and Device.DEFAULT in {"NV", "CUDA"}: return
-    def _test_value(n: float, unit: float = 1.0):
+    def _test_value(n: float, unit: float=1.0):
       next_float = np.nextafter(1.0, 2.0, dtype=_to_np_dtype(dtype))
       ulp = next_float - 1.0
       ulp = unit * ulp
       with Context(TRANSCENDENTAL=2):
-        np.testing.assert_allclose(
-          Tensor([n], dtype=dtype).log2().numpy(),
-          np.log2(np.array([n], dtype=_to_np_dtype(dtype))),
-          atol=ulp, rtol=1e-5)
+        np.testing.assert_allclose(Tensor([n], dtype=dtype).log2().numpy(), np.log2(np.array([n], dtype=_to_np_dtype(dtype))), atol=ulp, rtol=1e-5)
     fmin = np.finfo(_to_np_dtype(dtype)).tiny
     for scale in [1.0, 1e10, 1e20, 1e30]:
       _test_value(fmin * scale)
